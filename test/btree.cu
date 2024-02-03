12716da3e94ce87aa0377acb886f5127cf3be163
Yangshen Deng <yangshen.d@outlook.com>
2024-02-03 20:45:49 +0800

    update amac, comparative performance with zhaoyang's gp

--- test/btree.cu
+++ test/btree.cu
@@ -2,8 +2,8 @@
 #include <gtest/gtest.h>
 
 #include "btree/amac.cuh"
-#include "btree/gp.cuh"
 #include "btree/common.cuh"
+#include "btree/gp.cuh"
 #include "btree/naive.cuh"
 #include "datagen/generator_ETHZ.cuh"
 #include "util/args.cuh"
@@ -23,8 +23,8 @@ TEST(unique, naive) {
   fmt::print("Creating {} unique values ({} MB)\n", n,
              n * sizeof(int64_t) / 1024 / 1024);
 
-  int els_per_thread = 4;
-  int threads_per_block = 512;
+  int els_per_thread = 8; // 8/32 per warp is the best
+  int threads_per_block = 32;
   btree::Config config;
 
   const int els_per_block = threads_per_block * els_per_thread;
@@ -63,19 +63,18 @@ TEST(unique, amac) {
   // config.probe_gridsize = blocks_per_grid;
   // config.probe_blocksize = threads_per_block;
   config.probe_blocksize = 64;
-  config.probe_gridsize = 72;
+  config.probe_gridsize = 72 * 8;
   // config.probe_gridsize = 1;
   btree::amac::index(keys, values, n, config);
   fmt::print("Insert and Lookup {} tuples into BTree\n", n);
 }
-
 
 TEST(unique, gp) {
   int32_t n = args::get<int32_t>("N");
   std::string key_fname = cutil::rel_fname(true, "key_uniq", n, 0);
   std::string val_fname = cutil::rel_fname(true, "val_uniq", n, 0);
   int64_t *keys = new int64_t[n];
-  int64_t *values = new int64_t[n]; // ? keys == values
+  int64_t *values = new int64_t[n];  // ? keys == values
 
   assert(!datagen::create_relation_unique(key_fname.c_str(), keys, n, n));
   assert(!datagen::create_relation_unique(val_fname.c_str(), values, n, n));
@@ -85,8 +84,8 @@ TEST(unique, gp) {
   fmt::print("Creating {} unique values ({} MB)\n", n,
              n * sizeof(int64_t) / 1024 / 1024);
 
-  int els_per_thread = 8; // group size of GP ?
-  int threads_per_block = 8; //?
+  int els_per_thread = 8;     // group size of GP ?
+  int threads_per_block = 8;  //?
   btree::Config config;
 
   const int els_per_block = threads_per_block * els_per_thread;
