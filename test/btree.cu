9b9cf4e7ca4755bfc19ae603ca0ed9a693474f55
Yangshen Deng <yangshen.d@outlook.com>
2024-02-10 15:53:21 +0800

    implements imv as a modification of amac

--- test/btree.cu
+++ test/btree.cu
@@ -4,8 +4,9 @@
 #include "btree/amac.cuh"
 #include "btree/common.cuh"
 #include "btree/gp.cuh"
-#include "btree/spp.cuh"
+#include "btree/imv.cuh"
 #include "btree/naive.cuh"
+#include "btree/spp.cuh"
 #include "datagen/generator_ETHZ.cuh"
 #include "util/args.cuh"
 
@@ -35,6 +36,64 @@ TEST(unique, naive) {
   config.probe_gridsize = blocks_per_grid;
   config.probe_blocksize = threads_per_block;
   btree::naive::index(keys, values, n, config);
+  fmt::print("Insert and Lookup {} tuples into BTree\n", n);
+}
+
+TEST(unique, gp) {
+  int32_t n = args::get<int32_t>("N");
+  std::string key_fname = cutil::rel_fname(true, "key_uniq", n, 0);
+  std::string val_fname = cutil::rel_fname(true, "val_uniq", n, 0);
+  int64_t *keys = new int64_t[n];
+  int64_t *values = new int64_t[n];  // ? keys == values
+
+  assert(!datagen::create_relation_unique(key_fname.c_str(), keys, n, n));
+  assert(!datagen::create_relation_unique(val_fname.c_str(), values, n, n));
+
+  fmt::print("Creating {} unique keys ({} MB)\n", n,
+             n * sizeof(int64_t) / 1024 / 1024);
+  fmt::print("Creating {} unique values ({} MB)\n", n,
+             n * sizeof(int64_t) / 1024 / 1024);
+
+  int els_per_thread = 8;     // group size of GP ?
+  int threads_per_block = 8;  //?
+  btree::Config config;
+
+  const int els_per_block = threads_per_block * els_per_thread;
+  const int blocks_per_grid = (n + els_per_block - 1) / els_per_block;
+  config.build_gridsize = blocks_per_grid;
+  config.build_blocksize = threads_per_block;
+  config.probe_gridsize = blocks_per_grid;
+  config.probe_blocksize = threads_per_block;
+  btree::gp::index(keys, values, n, config);
+  fmt::print("Insert and Lookup {} tuples into BTree\n", n);
+}
+
+TEST(unique, spp) {
+  int32_t n = args::get<int32_t>("N");
+  std::string key_fname = cutil::rel_fname(true, "key_uniq", n, 0);
+  std::string val_fname = cutil::rel_fname(true, "val_uniq", n, 0);
+  int64_t *keys = new int64_t[n];
+  int64_t *values = new int64_t[n];
+
+  assert(!datagen::create_relation_unique(key_fname.c_str(), keys, n, n));
+  assert(!datagen::create_relation_unique(val_fname.c_str(), values, n, n));
+
+  fmt::print("Creating {} unique keys ({} MB)\n", n,
+             n * sizeof(int64_t) / 1024 / 1024);
+  fmt::print("Creating {} unique values ({} MB)\n", n,
+             n * sizeof(int64_t) / 1024 / 1024);
+
+  int els_per_thread = 8;     //
+  int threads_per_block = 8;  //
+  btree::Config config;
+
+  const int els_per_block = threads_per_block * els_per_thread;
+  const int blocks_per_grid = (n + els_per_block - 1) / els_per_block;
+  config.build_gridsize = blocks_per_grid;
+  config.build_blocksize = threads_per_block;
+  config.probe_gridsize = blocks_per_grid;
+  config.probe_blocksize = threads_per_block;
+  btree::spp::index(keys, values, n, config);
   fmt::print("Insert and Lookup {} tuples into BTree\n", n);
 }
 
@@ -70,37 +129,7 @@ TEST(unique, amac) {
   fmt::print("Insert and Lookup {} tuples into BTree\n", n);
 }
 
-TEST(unique, gp) {
-  int32_t n = args::get<int32_t>("N");
-  std::string key_fname = cutil::rel_fname(true, "key_uniq", n, 0);
-  std::string val_fname = cutil::rel_fname(true, "val_uniq", n, 0);
-  int64_t *keys = new int64_t[n];
-  int64_t *values = new int64_t[n];  // ? keys == values
-
-  assert(!datagen::create_relation_unique(key_fname.c_str(), keys, n, n));
-  assert(!datagen::create_relation_unique(val_fname.c_str(), values, n, n));
-
-  fmt::print("Creating {} unique keys ({} MB)\n", n,
-             n * sizeof(int64_t) / 1024 / 1024);
-  fmt::print("Creating {} unique values ({} MB)\n", n,
-             n * sizeof(int64_t) / 1024 / 1024);
-
-  int els_per_thread = 8;     // group size of GP ?
-  int threads_per_block = 8;  //?
-  btree::Config config;
-
-  const int els_per_block = threads_per_block * els_per_thread;
-  const int blocks_per_grid = (n + els_per_block - 1) / els_per_block;
-  config.build_gridsize = blocks_per_grid;
-  config.build_blocksize = threads_per_block;
-  config.probe_gridsize = blocks_per_grid;
-  config.probe_blocksize = threads_per_block;
-  btree::gp::index(keys, values, n, config);
-  fmt::print("Insert and Lookup {} tuples into BTree\n", n);
-}
-
-
-TEST(unique, spp) {
+TEST(unique, imv) {
   int32_t n = args::get<int32_t>("N");
   std::string key_fname = cutil::rel_fname(true, "key_uniq", n, 0);
   std::string val_fname = cutil::rel_fname(true, "val_uniq", n, 0);
@@ -115,16 +144,19 @@ TEST(unique, spp) {
   fmt::print("Creating {} unique values ({} MB)\n", n,
              n * sizeof(int64_t) / 1024 / 1024);
 
-  int els_per_thread = 8;     //
-  int threads_per_block = 8;  //
+  int els_per_thread = 4;
+  int threads_per_block = 256;
   btree::Config config;
 
   const int els_per_block = threads_per_block * els_per_thread;
   const int blocks_per_grid = (n + els_per_block - 1) / els_per_block;
   config.build_gridsize = blocks_per_grid;
   config.build_blocksize = threads_per_block;
-  config.probe_gridsize = blocks_per_grid;
-  config.probe_blocksize = threads_per_block;
-  btree::spp::index(keys, values, n, config);
+  // config.probe_gridsize = blocks_per_grid;
+  // config.probe_blocksize = threads_per_block;
+  config.probe_blocksize = 128;
+  config.probe_gridsize = 72 * 2;
+  // config.probe_gridsize = 1;
+  btree::imv::index(keys, values, n, config);
   fmt::print("Insert and Lookup {} tuples into BTree\n", n);
 }
