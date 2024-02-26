2eafa18d4e482221d53c09db8022c96dd6502b6d
hfhongzy <297775278@qq.com>
2024-02-26 13:20:47 +0800

    gp-sep upd

--- test/btree.cu
+++ test/btree.cu
@@ -4,11 +4,13 @@
 #include "btree/amac.cuh"
 #include "btree/common.cuh"
 #include "btree/gp.cuh"
+#include "btree/gp-sep.cuh"
 #include "btree/spp.cuh"
 #include "btree/naive.cuh"
 #include "datagen/generator_ETHZ.cuh"
 #include "util/args.cuh"
 
+/*
 TEST(unique, naive) {
   int32_t n = args::get<int32_t>("N");
   std::string key_fname = cutil::rel_fname(true, "key_uniq", n, 0);
@@ -70,6 +72,8 @@ TEST(unique, amac) {
   fmt::print("Insert and Lookup {} tuples into BTree\n", n);
 }
 
+*/
+
 TEST(unique, gp) {
   int32_t n = args::get<int32_t>("N");
   std::string key_fname = cutil::rel_fname(true, "key_uniq", n, 0);
@@ -99,7 +103,36 @@ TEST(unique, gp) {
   fmt::print("Insert and Lookup {} tuples into BTree\n", n);
 }
 
+TEST(unique, gp_sep) {
+  int32_t n = args::get<int32_t>("N");
+  std::string key_fname = cutil::rel_fname(true, "key_uniq", n, 0);
+  std::string val_fname = cutil::rel_fname(true, "val_uniq", n, 0);
+  int64_t *keys = new int64_t[n];
+  int64_t *values = new int64_t[n];  // ? keys == values
 
+  assert(!datagen::create_relation_unique(key_fname.c_str(), keys, n, n));
+  assert(!datagen::create_relation_unique(val_fname.c_str(), values, n, n));
+
+  fmt::print("Creating {} unique keys ({} MB)\n", n,
+             n * sizeof(int64_t) / 1024 / 1024);
+  fmt::print("Creating {} unique values ({} MB)\n", n,
+             n * sizeof(int64_t) / 1024 / 1024);
+
+  int els_per_thread = 8;     // group size of GP ?
+  int threads_per_block = 8;  //?
+  btree::Config config;
+
+  const int els_per_block = threads_per_block * els_per_thread;
+  const int blocks_per_grid = (n + els_per_block - 1) / els_per_block;
+  config.build_gridsize = blocks_per_grid;
+  config.build_blocksize = threads_per_block;
+  config.probe_gridsize = blocks_per_grid;
+  config.probe_blocksize = threads_per_block;
+  btree::gp_sep::index(keys, values, n, config);
+  fmt::print("Insert and Lookup {} tuples into BTree\n", n);
+}
+
+/*
 TEST(unique, spp) {
   int32_t n = args::get<int32_t>("N");
   std::string key_fname = cutil::rel_fname(true, "key_uniq", n, 0);
@@ -128,3 +161,4 @@ TEST(unique, spp) {
   btree::spp::index(keys, values, n, config);
   fmt::print("Insert and Lookup {} tuples into BTree\n", n);
 }
+*/
